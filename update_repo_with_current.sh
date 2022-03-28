91dd3dde5f3f80171470faf4f634269340c202e8
Nick Kuzmik <nick@kuzmik.org>
2022-03-28 15:53:00 -0400

    updated script

--- update_repo_with_current.sh
+++ update_repo_with_current.sh
@@ -6,7 +6,7 @@ set -o pipefail
 ## Actual dotfiles
 # .bashrc and .profile are symlinks to .bash_profile
 for f in aliases bash_history bash_profile gemrc gitconfig railsrc ripgreprc; do
-  cat "/Users/kuzmik/.$f" > dotfiles/$f
+  cat "~/.$f" > dotfiles/$f
 done
 
 ## Brew packages
@@ -16,13 +16,13 @@ brew bundle dump --all --describe --force
 mkdir -p Sublime/Packages/User
 
 # copy over installed packages because apparently packages can vanish, see boxy as an example
-cp -rf "/Users/kuzmik/Library/ApplicationSupport/Sublime Text 3/Installed Packages" Sublime
+cp -rf "~/Library/ApplicationSupport/Sublime Text 3/Installed Packages" Sublime
 
 # copy over boxy extra addons
-cp -rf "/Users/kuzmik/Library/ApplicationSupport/Sublime Text 3/Packages/Boxy Theme Addon - Mono File Icons" Sublime/Packages
-cp -rf "/Users/kuzmik/Library/ApplicationSupport/Sublime Text 3/Packages/Boxy Theme Addon - Unified Mode" Sublime/Packages
+cp -rf "~/Library/ApplicationSupport/Sublime Text 3/Packages/Boxy Theme Addon - Mono File Icons" Sublime/Packages
+cp -rf "~/Library/ApplicationSupport/Sublime Text 3/Packages/Boxy Theme Addon - Unified Mode" Sublime/Packages
 
 # copy over the sublime settings from user
 for f in "Package Control.sublime-settings" "Preferences.sublime-settings" "Default (OSX).sublime-keymap"; do
-  cp -rf "/Users/kuzmik/Library/ApplicationSupport/Sublime Text 3/Packages/User/$f" Sublime/Packages/User
+  cp -rf "~/Library/ApplicationSupport/Sublime Text 3/Packages/User/$f" Sublime/Packages/User
 done
